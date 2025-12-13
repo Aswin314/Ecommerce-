d8ee619e0799604cdebe59154e3c70668a93f17b
Aswin <aswinanand314@gmail.com>
2025-12-13 12:15:01 +0530

    tailwind added

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
-
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import ConnectDB from "./config/db.js";
@@ -12,8 +11,10 @@ ConnectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
-
+app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
 app.listen(PORT, () => {
-  console.log(`http://localhost${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
